cf3a122a3804b4cc3584c43f6ae145e0433da87c
perissinotti <perissinotti@gmail.com>
2015-10-06 15:58:23 -0600

    asking filename

--- extract.sh
+++ extract.sh
@@ -1,4 +1,4 @@
 #Laura Perissinotti, October 6, 2015
 #This script will extract the changes from the thing
 
-cut -f 2 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv 
+cut add name -f 2 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv 
